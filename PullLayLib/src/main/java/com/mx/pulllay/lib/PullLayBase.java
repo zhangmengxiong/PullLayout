4ec6e0330b6fedbaa0b4bc5c5a77eac5db5afdc4
zmx_f <zmx_final@163.com>
2017-03-31 16:10:49 +0800

    Initial commit

---
+++ PullLayLib/src/main/java/com/mx/pulllay/lib/PullLayBase.java
@@ -0,0 +1,11 @@
+package com.mx.pulllay.lib;
+
+/**
+ * 创建人： zhangmengxiong
+ * 创建时间： 2017/3/31.
+ * 联系方式: zmx_final@163.com
+ */
+
+public abstract class PullLayBase {
+
+}
---
+++ app/src/main/java/com/mx/pull_lay/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mx.pull_lay;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
